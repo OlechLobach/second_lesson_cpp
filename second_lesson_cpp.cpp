4454a862950f6d1fbaca78e17dfc90005b2607cd
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-26 22:20:40 +0300

    initial commit

--- second_lesson_cpp.cpp
+++ second_lesson_cpp.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
-
+#include<Windows.h>
 int main() {
+    system("chcp 1251");
+    system("cls");
+    int year;
+    std::cout << "Ââåä³òü ð³ê: ";
+    std::cin >> year;
+    bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 
+    
+    int daysInYear = isLeapYear ? 366 : 365;
+
+    std::cout << "Ó ðîö³ " << year << " º " << daysInYear << " äí³â." << std::endl;
 	return 0;
 }
