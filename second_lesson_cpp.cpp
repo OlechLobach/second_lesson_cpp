d21b7a77d5463d9c0a630638d508bbd325589fd1
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-28 01:55:55 +0300

    initial commit

--- second_lesson_cpp.cpp
+++ second_lesson_cpp.cpp
@@ -1,6 +1,20 @@
-#include<iostream>
+#include <iostream>
 
 int main() {
+    double v; 
+    double t; 
+    double a;
 
-	return 0;
+    std::cout << "¬вед≥ть швидк≥сть (м/с): ";
+    std::cin >> v;
+    std::cout << "¬вед≥ть час (сек): ";
+    std::cin >> t;
+    std::cout << "¬вед≥ть прискоренн€ (м/с^2): ";
+    std::cin >> a;
+
+    double S = v * t + (a * t * t) / 2;
+
+    std::cout << "ѕройдена в≥дстань: " << S << " метр≥в" << std::endl;
+
+    return 0;
 }
