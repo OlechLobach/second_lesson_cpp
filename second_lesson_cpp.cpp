f93c65507d0b6f1e11aed04b2b93b772487490d4
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-28 01:43:49 +0300

    initial commit

--- second_lesson_cpp.cpp
+++ second_lesson_cpp.cpp
@@ -1,6 +1,20 @@
-#include<iostream>
+#include <iostream>
 
 int main() {
+    system("chcp 1251");
+    system("cls");
+    double R1, R2, R3;
 
-	return 0;
+    std::cout << "Ââåä³òü îï³ð R1: ";
+    std::cin >> R1;
+    std::cout << "Ââåä³òü îï³ð R2: ";
+    std::cin >> R2;
+    std::cout << "Ââåä³òü îï³ð R3: ";
+    std::cin >> R3;
+
+    double R0 = 1 / (1 / R1 + 1 / R2 + 1 / R3);
+
+    std::cout << "Îï³ð R0: " << R0 << std::endl;
+
+    return 0;
 }
