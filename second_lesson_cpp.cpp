b3ac3dfb5767c6e1493c10675a430736a2a33144
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-28 01:31:20 +0300

    initial commit

--- second_lesson_cpp.cpp
+++ second_lesson_cpp.cpp
@@ -1,6 +1,20 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
+#include <Windows.h>
+
+const double pi = 3.14159265358979323846;
 
 int main() {
+    system("chcp 1251");
+    system("cls");
+    double radius;
 
-	return 0;
+    std::cout << "ќбчисленн€ об'Їму кул≥" << std::endl;
+    std::cout << "¬вед≥ть рад≥ус кул≥ (у сантиметрах): ";
+    std::cin >> radius;
+    double volume = (4.0 / 3.0) * pi * std::pow(radius, 3);
+
+    std::cout << "ќб'Їм кул≥ з рад≥усом " << radius << " см: " << volume << " куб. см" << std::endl;
+
+    return 0;
 }
