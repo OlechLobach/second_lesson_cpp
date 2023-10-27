13b46d701b34f4a9314aa3afa9950b3604a8986c
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-28 01:20:02 +0300

    initial commit

--- second_lesson_cpp.cpp
+++ second_lesson_cpp.cpp
@@ -1,6 +1,21 @@
-#include<iostream>
+#include <iostream>
 
 int main() {
+    double mapScale, distanceOnMap;
+    system("chcp 1251");
+    system("cls");
+    std::cout << "Обчислення відстані між населеними пунктами" << std::endl;
+    std::cout << "Введіть вихідні дані:" << std::endl;
 
-	return 0;
+    std::cout << "Масштаб карти (кількість кілометрів в одному сантиметрі) -> ";
+    std::cin >> mapScale;
+
+    std::cout << "Відстань між точками, що зображують населені пункти (см) -> ";
+    std::cin >> distanceOnMap;
+
+    double distanceInKm = distanceOnMap / mapScale;
+
+    std::cout << "Відстань між населеними пунктами " << distanceInKm << " км." << std::endl;
+
+    return 0;
 }
