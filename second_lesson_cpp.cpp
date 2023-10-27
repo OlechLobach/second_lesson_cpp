4fb4647d4aeccab35b663d41b73168e2c1386630
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-28 00:40:00 +0300

    initial commit

--- second_lesson_cpp.cpp
+++ second_lesson_cpp.cpp
@@ -1,6 +1,22 @@
-#include<iostream>
+#include <iostream>
 
 int main() {
+    system("chcp 1251");
+    system("cls");
+    int hryvnia, kopecks;
 
-	return 0;
+    std::cout << "Введіть суму в гривнях: ";
+    std::cin >> hryvnia;
+
+    std::cout << "Введіть суму в копійках: ";
+    std::cin >> kopecks;
+
+    if (kopecks >= 100) {
+        hryvnia += kopecks / 100;
+        kopecks %= 100;
+    }
+
+    std::cout << "Коректована сума: " << hryvnia << " грн. " << kopecks << " коп." << std::endl;
+
+    return 0;
 }
