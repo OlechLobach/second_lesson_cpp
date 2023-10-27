55f9f99adfdf2e1450515b7aa0aed71bfd9b0a53
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-28 00:54:49 +0300

    initial commit

--- second_lesson_cpp.cpp
+++ second_lesson_cpp.cpp
@@ -1,6 +1,25 @@
-#include<iostream>
+#include <iostream>
 
 int main() {
+    double length, width, height;
+    system("chcp 1251");
+    system("cls");
 
-	return 0;
+    std::cout << "Обчислення об'єму паралелепіпеда" << std::endl;
+    std::cout << "Введіть вихідні дані:" << std::endl;
+
+    std::cout << "Довжина (см) -> ";
+    std::cin >> length;
+
+    std::cout << "Ширина (см) -> ";
+    std::cin >> width;
+
+    std::cout << "Висота (см) -> ";
+    std::cin >> height;
+
+    double volume = length * width * height;
+
+    std::cout << "Об'єм: " << volume << " куб.см." << std::endl;
+
+    return 0;
 }
